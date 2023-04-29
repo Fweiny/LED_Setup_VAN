2f6bb0f29a7b54156ae3589e7add9598bc2079c6
Fweiny <florian@weinzierlhome.de>
2023-04-29 12:15:55 +0200

    PWM working 1 btn

--- main.py
+++ main.py
@@ -5,18 +5,16 @@ from time import sleep
 #functions
 def Btn_function(LED,btn_on_time):          
     global doppelklick, duty_cycle
-    global status_F,status_M, status_B
-
     #print(f'{LED.duty()=}')
 
     #shutting on/off
-    if (doppelklick is False) &  (btn_on_time == 0):
+    if (doppelklick is False) & (btn_on_time == 0):
         if (LED.duty() == 0):
-            print(f'Shut On - Single ')
+            print('Lights on - Single')
             #duty_cycle = duty_cycle_start
             LED.duty(duty_cycle)
-        elif (duty_cycle >0):
-            print(f'Shut Off - Single')
+        else:
+            print('Lights off - Single')
             LED.duty(0)      
     elif (btn_on_time == 0):
         doppelklick = False
@@ -46,13 +44,13 @@ def dimmen():
     # Change duty 
     duty_cycle = duty_cycle - step
 
-    if (duty_cycle >0):
+    if (LED_F.duty() > 0):
         LED_F.duty(duty_cycle)
 
-    if (duty_cycle >0):
+    if (LED_M.duty() > 0):
         LED_M.duty(duty_cycle)
 
-    if (duty_cycle >0):
+    if (LED_B.duty() > 0):
         LED_B.duty(duty_cycle)
 
     sleep(dimm_sleep_time)
@@ -68,40 +66,30 @@ def dimmen():
 
 # LED_F settings
 frequency = 800
-
-
-LED_F = PWM(Pin(17), frequency)  #front 17
-LED_M = PWM(Pin(16), frequency)  # 16 Mid
-LED_B = PWM(Pin(2), frequency) #Back 2
-
 duty_cycle = 800
-duty_cycle_start = 800
 duty_max = 800
 
+
+LED_F = PWM(Pin(17), frequency)         # Front Pin 17
+LED_M = PWM(Pin(16), frequency)         # Mid   Pin 16
+LED_B = PWM(Pin(2), frequency)          # Back  Pin 2
+
+
 #Button definition
-btn_F = Pin(22, Pin.IN, Pin.PULL_DOWN) # front 22
-btn_M = Pin(19, Pin.IN, Pin.PULL_DOWN) # Mid 19
-btn_B = Pin(34, Pin.IN, Pin.PULL_DOWN) #bak 15
+btn_F = Pin(22, Pin.IN, Pin.PULL_DOWN) # Front  Pin 22
+btn_M = Pin(19, Pin.IN, Pin.PULL_DOWN) # Mid    Pin 19
+btn_B = Pin(34, Pin.IN, Pin.PULL_DOWN) # Back   Pin 15
 
-
-#status of the lights 
-status_all = False # LED all 
-status_F = False  #LED status_F
-status_M = False  #LED status_F
-status_B = False  #LED status_F
-
-
-sleep_time = .01
 
 # Dimm settings
-dimm_sleep_time = .1 # timer for dimming
-step = 25 #PWM step for dimming
-doppelklick_timer = 1 # break between klicks
-doppelklick = False
+sleep_time = .01            # Sleep on timer
+dimm_sleep_time = .1        # Timer for dimming
+step = 25                   # PWM step for dimming
+doppelklick_timer = 1       # Break between klicks
+doppelklick = False         # Req. 
 
 # start timer - default
 t_delta = 10
-
 timestamp_prev_F = get_timestamp()
 timestamp_F = get_timestamp()
 btn_count_F = 0
@@ -109,29 +97,22 @@ off_count_F = True
 btn_on_time_F = 0
 
 
-
-
-
-
-# Start turn all off
+# Start turn all off as start up
 LED_F.duty(0)
 LED_M.duty(0)
 LED_B.duty(0)
 
+#LED_Dict={LED_F:"Front",LED_B:"Back",LED_M:"Mid"}
+
 
 if __name__ == '__main__':
-
-
     while True:
+
         if btn_F.value() == 1:
             if off_count_F is True:
                 print("---------------------")
-                # get timestmap 
-                timestamp_F = get_timestamp()
-                #print(f'{timestamp=}')
-
-                # calc delta time
-                t_delta = timestamp_F - timestamp_prev_F
+                timestamp_F = get_timestamp()  # get timestmap 
+                t_delta = timestamp_F - timestamp_prev_F # calc delta time
                 #print(f'{t_delta=}')
 
                 # count number of clicks (depending on time delta)
@@ -142,7 +123,7 @@ if __name__ == '__main__':
                 elif (btn_count_F == 0): # increase for buttun push
                     btn_count_F = 1
 
-                #print(f"{btn_count=}")
+            #print(f"{btn_count=}")
 
             #Button fuctnion
             Btn_function(LED_F,btn_on_time_F)
@@ -151,15 +132,12 @@ if __name__ == '__main__':
             if (btn_on_time_F >= 100):
                 dimmen()
 
-            btn_on_time_F = btn_on_time_F + 1   # increasing depending on Actuator on time
-            sleep(sleep_time)                   # delay 
+            btn_on_time_F = btn_on_time_F + 1   # increasing depending on Actuator on time  # noqa: E501
             off_count_F = False                 # ensuring on time
+            sleep(sleep_time)                   # delay 
         else:
-            #reset dimm counter 
-            btn_on_time_F = 0
+            btn_on_time_F = 0                   # reset dimm counter 
             off_count_F = True
-
-            # reset timer for new klick
-            timestamp_prev_F = timestamp_F
+            timestamp_prev_F = timestamp_F       # reset timer for new klick
 
            
